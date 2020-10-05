522ac6c0227732d3f11cf0acd49c94ce3abc4038
Daniel Negrisoli Batista <danielnegrisoli@gmail.com>
2020-10-05 20:51:06 -0300

    Added ApplicationCustomerService, ApplicationProductService

---
+++ DDDStore/DDDStore.Application/Services/ApplicationCustomerService.cs
@@ -0,0 +1,38 @@
+﻿using DDDStore.Application.DTO;
+using DDDStore.Application.Interfaces;
+using DDDStore.Domain.Core.Interfaces.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DDDStore.Application.Services
+{
+    public class ApplicationCustomerService : IApplicationCustomerService
+    {
+        private readonly ICustomerService _customerService;
+        public void Add(CustomerDTO customerDTO)
+        {
+            throw new NotImplementedException();
+        }
+        public IEnumerable<CustomerDTO> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+        public CustomerDTO GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+        public void Remove(CustomerDTO customerDTO)
+        {
+            throw new NotImplementedException();
+        }
+        public void Update(CustomerDTO customerDTO)
+        {
+            throw new NotImplementedException();
+        }
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DDDStore/DDDStore.Application/Services/ApplicationProductService.cs
@@ -0,0 +1,43 @@
+﻿using DDDStore.Application.DTO;
+using DDDStore.Application.Interfaces;
+using DDDStore.Domain.Core.Interfaces.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DDDStore.Application.Services
+{
+    public class ApplicationProductService : IApplicationProductService
+    {
+        private readonly ICustomerService _customerService;
+
+        public ApplicationProductService(ICustomerService customerService)
+        {
+            _customerService = customerService;
+        }
+        public void Add(ProductDTO productDTO)
+        {
+            throw new NotImplementedException();
+        }
+        public IEnumerable<ProductDTO> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+        public ProductDTO GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+        public void Remove(ProductDTO productDTO)
+        {
+            throw new NotImplementedException();
+        }
+        public void Update(ProductDTO productDTO)
+        {
+            throw new NotImplementedException();
+        }
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
