5a246bd1ea79b5e28ffc6be0bbd1135278bcbf0c
Daniel Negrisoli Batista <danielnegrisoli@gmail.com>
2020-10-05 20:39:10 -0300

    Added Application.Interfaces.Services

---
+++ DDDStore/DDDStore.Application/Interfaces/IApplicationCustomerService.cs
@@ -0,0 +1,17 @@
+﻿using DDDStore.Application.DTO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DDDStore.Application.Interfaces
+{
+    interface IApplicationCustomerService
+    {
+        void Add(CustomerDTO customerDTO);
+        CustomerDTO GetById(int id);
+        IEnumerable<CustomerDTO> GetAll();
+        void Update(CustomerDTO customerDTO);
+        void Remove(CustomerDTO customerDTO);
+        void Dispose();
+    }
+}
---
+++ DDDStore/DDDStore.Application/Interfaces/IApplicationProductService.cs
@@ -0,0 +1,17 @@
+﻿using DDDStore.Application.DTO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DDDStore.Application.Interfaces
+{
+    interface IApplicationProductService
+    {
+        void Add(ProductDTO productDTO);
+        ProductDTO GetById(int id);
+        IEnumerable<ProductDTO> GetAll();
+        void Update(ProductDTO productDTO);
+        void Remove(ProductDTO productDTO);
+        void Dispose();
+    }
+}
