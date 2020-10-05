2b5eeb3a279fd6f785b143f7bf041248c869d7ce
Daniel Negrisoli Batista <danielnegrisoli@gmail.com>
2020-10-05 19:57:32 -0300

    Added Core.Interface.Services

---
+++ DDDStore/DDDStore.Domain.Core/Interfaces/Services/IBaseService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DDDStore.Domain.Core.Interfaces.Services
+{
+    public interface IBaseService<TEntity> where TEntity : class
+    {
+        void Add(TEntity entity);
+        TEntity GetById(int id);
+        IEnumerable<TEntity> GetAll();
+        void Update(TEntity entity);
+        void Remove(TEntity entity);
+    }
+}
---
+++ DDDStore/DDDStore.Domain.Core/Interfaces/Services/IOrderService.cs
@@ -0,0 +1,11 @@
+﻿using DDDStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DDDStore.Domain.Core.Interfaces.Services
+{
+    public interface IOrderService : IBaseService<Order>
+    {
+    }
+}
