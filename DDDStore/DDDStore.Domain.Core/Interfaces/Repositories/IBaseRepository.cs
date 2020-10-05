833ff35dd7746ac5bb704ecf26aaf93bb123a185
Daniel Negrisoli Batista <danielnegrisoli@gmail.com>
2020-10-05 19:57:17 -0300

    Added Core.Interface.Repositories

---
+++ DDDStore/DDDStore.Domain.Core/Interfaces/Repositories/IBaseRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DDDStore.Domain.Core.Interfaces.Repositories
+{
+    public interface IBaseRepository<TEntity> where TEntity : class
+    {
+        void Add(TEntity entity);
+        TEntity GetById(int id);
+        IEnumerable<TEntity> GetAll();
+        void Update(TEntity entity);
+        void Remove(TEntity entity);
+        void Dispose();
+    }
+}
---
+++ DDDStore/DDDStore.Domain.Core/Interfaces/Repositories/IOrderRepository.cs
@@ -0,0 +1,11 @@
+﻿using DDDStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DDDStore.Domain.Core.Interfaces.Repositories
+{
+    public interface IOrderRepository : IBaseRepository<Order>
+    {
+    }
+}
